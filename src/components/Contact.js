c5f58e011163247aa07db46b9e52318251c455fe
Darcey Axon <darceyaxon@darceys-air.lan>
2023-12-03 22:09:47 -0800

    add contact form and more styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,5 +2,22 @@ import React, { useState } from 'react';
 
 
 export function Contact() {
-    return <h1> Contact Us </h1>
-}
+    return (
+      <div className='homeTitle'>
+        <h1>Contact Us</h1>
+  
+        <div className="contactUs">
+          <form>
+            <label>Name</label>
+            <input placeholder="Enter Name"></input><br></br>
+            <label>Email</label>
+            <input placeholder="Enter Email Address"></input><br></br>
+            <label>Cell Phone Number</label>
+            <input placeholder="Enter Cell Phone Number"></input><br></br>
+            <label></label>
+            <button className="editButton">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
